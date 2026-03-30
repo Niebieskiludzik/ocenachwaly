995617f0e1651f0e2306e24588bded004b81c03f
Niebieskiludzik <minecraftblueboy7@gmail.com>
2026-03-30 17:47:00 +0200

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,3 @@
+const supabaseUrl = 'https://wzanqzcjrpbhocrfcciy.supabase.co',
+const supabaseUrl = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind6YW5xemNqcnBiaG9jcmZjY2l5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE0MzQ4MjUsImV4cCI6MjA4NzAxMDgyNX0.VNer3odvLPJzBbecICFZFw86SXvvCbEZDQNVciEm97k';
+window.supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
